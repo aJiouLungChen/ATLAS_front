0b2a77b8fe3e6962f193e6073892cf4c41b84aca
aJiouLungChen <jiou-lung.chen@access-company.com>
2022-08-29 14:50:52 +0800

    [AT-87] desprate the some constant to app_configurations from app_texts

---
+++ client/lib/src/constants/app_configurations.dart
@@ -0,0 +1,11 @@
+class AppConfigurations{
+  static const String kImgPath = "images/btn_google_light_normal_ios.svg";
+  static const String kAuthority = "accounts.google.com";
+  static const String kUnEncodePath = "/o/oauth2/v2/auth";
+  static const String kResponseType = "code";
+  static const String kClientId =
+      "1068831372506-eqpgvmp11kj2rcdh9ce9of169pvbn8np.apps.googleusercontent.com";
+  static const String kRedirectUri = "http://localhost:58702/auth";
+  static const String kScope = "profile email";
+  static const String kFlowName = "GeneralOAuthFlow";
+}
