feaa94ba7cf107a0f9b0dcd41a54579f814caa9d
aJiouLungChen <jiou-lung.chen@access-company.com>
2022-08-29 15:09:24 +0800

    [AT-87] revise the auth code name to authCode from code and do a few modify

--- client/lib/src/ui/auth_page/auth_page.dart
+++ client/lib/src/ui/auth_page/auth_page.dart
@@ -9,28 +9,28 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  late String code;
+  late String authCode;
 
   @override
   void initState() {
     super.initState();
     Uri url = Uri.parse(window.location.href);
-    code = url.queryParameters["code"].toString();
+    authCode = url.queryParameters["code"].toString();
     //NetworkingService().fetchUserInfo(code);
   }
 
   @override
   Widget build(BuildContext context) {
     return const Center(
-        child: SizedBox(
-            width: 150,
-            height: 150,
-            child: CircularProgressIndicator(
-              backgroundColor: Colors.grey,
-              strokeWidth: 15,
-              color: Colors.black,
-            ))
-        // child: SelectableText(code),
-        );
+      child: SizedBox(
+        width: 150,
+        height: 150,
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.grey,
+          strokeWidth: 15,
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 }
--- client/lib/src/ui/entrance_page/entrance_page.dart
+++ client/lib/src/ui/entrance_page/entrance_page.dart
@@ -2,7 +2,7 @@ import 'package:client/src/ui/entrance_page/widgets/google_sign_in_button.dart';
 import 'package:client/src/ui/entrance_page/widgets/logo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:client/src/constants/app_texts.dart';
+import 'package:client/src/constants/app_configurations.dart';
 
 class EntrancePage extends StatefulWidget {
   const EntrancePage({Key? key}) : super(key: key);
@@ -12,7 +12,7 @@ class EntrancePage extends StatefulWidget {
 }
 
 class _EntrancePageState extends State<EntrancePage> {
-  final Widget googleSignInIconSvg = SvgPicture.asset(AppTexts.kImgPath);
+  final Widget googleSignInIconSvg = SvgPicture.asset(AppConfigurations.kImgPath);
 
   @override
   Widget build(BuildContext context) {
